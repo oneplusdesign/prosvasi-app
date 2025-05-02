61cf80667159e1b24839907fd78af2fd401a6d2b
oneplusdesign <38374259+oneplusdesign@users.noreply.github.com>
2025-05-02 11:58:10 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
 // Home Page
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Καλώς ήρθατε στην εφαρμογή προσβασιμότητας</h1>
+      <p>Αυτή η εφαρμογή δείχνει σημεία προσβασιμότητας στην πόλη του Αγρινίου.</p>
+    </div>
+  );
+}
