7aa59555d8a86f6d0ba02e848a7c35ceebb76735
oneplusdesign <38374259+oneplusdesign@users.noreply.github.com>
2025-05-02 19:51:23 +0300

    Add files via upload

--- pages/add-drag.js
+++ pages/add-drag.js
@@ -1,8 +1,5 @@
-// pages/add-drag.js
 import dynamic from 'next/dynamic';
-
 const AddDragView = dynamic(() => import('../components/AddDragView'), { ssr: false });
-
-export default function AddDragWrapper() {
+export default function AddDragPage() {
   return <AddDragView />;
 }
--- pages/map.js
+++ pages/map.js
@@ -1,8 +1,5 @@
-// pages/map.js
 import dynamic from 'next/dynamic';
-
 const MapView = dynamic(() => import('../components/MapView'), { ssr: false });
-
 export default function MapPage() {
   return <MapView />;
 }
