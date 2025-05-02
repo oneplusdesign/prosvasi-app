c5def601150ccc511503266d75bd6f1325fac858
oneplusdesign <38374259+oneplusdesign@users.noreply.github.com>
2025-05-02 13:26:43 +0300

    Update locations.js

--- pages/api/locations.js
+++ pages/api/locations.js
@@ -1,3 +1,9 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Locations' });
+// pages/api/locations.js
+import dbConnect from '@/utils/db';
+
+export default async function handler(req, res) {
+  await dbConnect();
+
+  // Εδώ πρόσθεσε την MongoDB λογική σου, π.χ. insert/find κλπ
+  res.status(200).json({ message: 'Connected to MongoDB successfully!' });
 }
