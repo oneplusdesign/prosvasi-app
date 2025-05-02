aba36fd4631d78a3f4ed3200bea1f448e32690cf
oneplusdesign <38374259+oneplusdesign@users.noreply.github.com>
2025-05-02 13:52:43 +0300

    Update map.js

--- pages/map.js
+++ pages/map.js
@@ -1,31 +1,13 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
-import { useEffect, useState } from 'react';
+// pages/map.js
+import dynamic from 'next/dynamic';
 
-export default function Map() {
-  const [locations, setLocations] = useState([]);
+const Map = dynamic(() => import('../components/Map'), { ssr: false });
 
-  useEffect(() => {
-    fetch('/api/locations')
-      .then((res) => res.json())
-      .then((data) => setLocations(data));
-  }, []);
-
+export default function MapPage() {
   return (
-    <MapContainer center={[38.6248, 21.4064]} zoom={14} style={{ height: '100%', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
-      />
-      {locations.map((loc, i) => (
-        <Marker key={i} position={[loc.lat, loc.lng]}>
-          <Popup>
-            <strong>{loc.name}</strong>
-            <br />
-            {loc.description}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div>
+      <h1 style={{ padding: '1rem' }}>Χάρτης Προσβασιμότητας</h1>
+      <Map />
+    </div>
   );
 }
