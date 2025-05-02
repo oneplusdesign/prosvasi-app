d3fbdda914ed9fef754167a425f67e813ce998d6
oneplusdesign <38374259+oneplusdesign@users.noreply.github.com>
2025-05-02 13:43:10 +0300

    Create map.js

---
+++ pages/map.js
@@ -0,0 +1,31 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import { useEffect, useState } from 'react';
+
+export default function Map() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/locations')
+      .then((res) => res.json())
+      .then((data) => setLocations(data));
+  }, []);
+
+  return (
+    <MapContainer center={[38.6248, 21.4064]} zoom={14} style={{ height: '100%', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      {locations.map((loc, i) => (
+        <Marker key={i} position={[loc.lat, loc.lng]}>
+          <Popup>
+            <strong>{loc.name}</strong>
+            <br />
+            {loc.description}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
