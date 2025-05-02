59b038d2cfa9df0cae83d5e1de439e3203674e69
oneplusdesign <38374259+oneplusdesign@users.noreply.github.com>
2025-05-02 20:06:56 +0300

    Update map.js

--- pages/map.js
+++ pages/map.js
@@ -1,5 +1,7 @@
+// pages/map.js
 import dynamic from 'next/dynamic';
 const MapView = dynamic(() => import('../components/MapView'), { ssr: false });
+
 export default function MapPage() {
   return <MapView />;
 }
