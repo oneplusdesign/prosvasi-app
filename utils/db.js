42a2a653b05d8de2b1b432dd43d232eab9c46056
oneplusdesign <38374259+oneplusdesign@users.noreply.github.com>
2025-05-02 13:29:34 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable in Vercel settings.');
+  throw new Error('Please define the MONGODB_URI environment variable inside Vercel settings');
 }
 
 let cached = global.mongoose;
@@ -14,13 +14,13 @@ if (!cached) {
 }
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
       bufferCommands: false,
+    }).then((mongoose) => {
+      return mongoose;
     });
   }
 
